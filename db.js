1cb0fcde4096a901779851cf0dc8e3384dbd4fe7
Naman Chaturvedi <namanchaturvedi58@gmail.com>
2025-01-01 14:59:19 +0530

    first commit

---
+++ db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL, {
+            useMongoClient: true,
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            
+        });
+        console.log(
+            `Conneted To Mongodb Databse ${conn.connection.host}`
+        );
+    } catch (error) {
+        console.log(`Errro in Mongodb ${error}`);
+    }
+};
+
+export default connectDB;
